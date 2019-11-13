88a99542d2b5c4adfbf725a097c307a71807d010
deeplook <gherman@darwin.in-berlin.de>
2019-11-13 22:47:23 +0100

    Update jupyter_notebook_config.py

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -2,5 +2,9 @@ c.ServerProxy.servers = {
   'DirectoryBrowser': {
     'command': ['python3', '-m', 'http.server', '{port}'],
     'absolute_url': False
+  },
+  'Flask': {
+    'command': ['flask', 'run', '--port', '{port}'],
+    'absolute_url': False
   }
 }
