9d724b3bacd70730bc1b0ce9770c926940a21873
deeplook <gherman@darwin.in-berlin.de>
2019-11-20 15:09:10 +0100

    Create index.js

---
+++ harpgl/example2/index.js
@@ -0,0 +1,26 @@
+// an globe with HERE vector tiles
+
+const canvas = document.getElementById("map");
+const map = new harp.MapView({
+  canvas,
+  theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
+  maxVisibleDataSourceTiles: 40,
+  tileCacheSize: 100,
+  projection: harp.sphereProjection
+});
+
+map.setCameraGeolocationAndZoom(new harp.GeoCoordinates(38.736946, -9.142685), 16);
+
+const mapControls = new harp.MapControls(map);
+const ui = new harp.MapControlsUI(mapControls);
+canvas.parentElement.appendChild(ui.domElement);
+
+window.onresize = () => map.resize(window.innerWidth, window.innerHeight);
+
+const omvDataSource = new harp.OmvDataSource({
+  baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
+  apiFormat: harp.APIFormat.XYZOMV,
+  styleSetName: "tilezen",
+  authenticationCode: "AFtI2oC2WEe2pZN2MOmD0j4"
+});
+map.addDataSource(omvDataSource);
